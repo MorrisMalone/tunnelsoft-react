eafc633abd40261d9984d942489fc204e8849e8b
johan morin <johan.morin@hotmail.fr>
2024-06-03 13:44:58 +0200

    add fetch options

--- src/utils/fetchImage.ts
+++ src/utils/fetchImage.ts
@@ -1,5 +1,5 @@
-export default async function fetchImage(url: string): Promise<string> {
-    const response = await fetch(url);
+export default async function fetchImage(url: string, fetchOptions: RequestInit): Promise<string> {
+    const response = await fetch(url, fetchOptions);
     const blob = await response.blob();
     const objectUrl = URL.createObjectURL(blob);
     return objectUrl;
